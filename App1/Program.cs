48f889647d0502a45ec708e6863a1514643a992c
YuriyFedosov <92025361+YuriyFedosov@users.noreply.github.com>
2021-11-24 01:07:27 +0300

    Добавьте файлы проекта.

---
+++ App1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace App1
+{
+    class Program
+    {
+        /*Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.*/
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Введите значение N для расчета факториала - ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Значение N! = {FactCalclAsync(n).Result}");
+                Console.ReadKey();
+            }
+            
+        
+            
+        }
+   
+
+        static async Task<int> FactCalclAsync(int n) //Метод для асинхронного расчета
+        {
+            int res = await Task.Run(() => FactCalc(n)); //Асинхронный запуск
+            return res;
+        }
+        static int FactCalc(int n) //Расчетный метод
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * FactCalc(n - 1); // Рекурсивный вызов функции
+        }
+    }
+}
